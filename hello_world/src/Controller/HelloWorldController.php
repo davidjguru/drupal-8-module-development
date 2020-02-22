00fd424573c6d87fefa59d92856714379c247d0e
davidjguru <davidjguru@gmail.com>
2020-02-22 01:04:48 +0100

    (CFG) / (PHP) - Add examples about routing, services and dependency injection.

--- hello_world/src/Controller/HelloWorldController.php
+++ hello_world/src/Controller/HelloWorldController.php
@@ -3,11 +3,37 @@
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\hello_world\HelloWorldSalutation;
+use Drupal\user\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Controller for the salutation message.
  */
 class HelloWorldController extends ControllerBase{
+
+  /**
+   * @var \Drupal\hello_world\HelloWorldSalutation
+   */
+  protected $salutation;
+
+  /**
+   * HelloWorldController constructor.
+   *
+   * @param \Drupal\hello_world\HelloWorldSalutation $salutation
+   */
+  public function __construct(HelloWorldSalutation $salutation){
+    $this->salutation = $salutation;
+  }
+
+  /**
+   * { @inheritdoc }
+   */
+  public static function create(ContainerInterface $container){
+    return new static(
+      $container->get('hello_world.salutation')
+    );
+}
 
   /**
    * Hello World.
@@ -16,10 +42,43 @@ class HelloWorldController extends ControllerBase{
    */
   public function helloWorld() {
     return [
-      '#markup' => $this->t('Hello World'),
+      '#markup' => $this->t('Hello World') . '<br>' .
+                   $this->salutation->getSalutation(),
     ];
   }
 
+  /**
+   * Hello Name.
+   *
+   * @return array
+   */
+  public function helloName($name) {
+    return [
+      '#markup' => $this->t('Hello World, dear: ' . $name),
+    ];
+  }
 
+  /**
+   * Hello User.
+   *
+   * @return array
+   */
+  public function helloUser(UserInterface $user) {
+    return [
+      '#markup' => $this->t('Hello User: @user',
+                   ['@user' => $user->getDisplayName()]),
+    ];
+  }
+
+  /**
+   * Hello Name Default.
+   *
+   * @return array
+   */
+  public function helloNameDefault($name) {
+    return [
+      '#markup' => $this->t('Hello World, dear: ' . $name),
+    ];
+  }
 
 }
