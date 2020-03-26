e7c489954acf06c536317c0dfb6b3dd3ff7b62ce
davidjguru <davidjguru@gmail.com>
2020-03-26 18:55:52 +0100

    (PHP) / (YML) - Adds a new Event Dispatcher for the Hello World Salutation Service.

--- hello_world/src/HelloWorldSalutation.php
+++ hello_world/src/HelloWorldSalutation.php
@@ -4,6 +4,7 @@ namespace Drupal\hello_world;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Prepares the salutation to the world.
@@ -11,6 +12,11 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
 class HelloWorldSalutation {
 
   use StringTranslationTrait;
+
+  /**
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
 
   /**
    * @var \Drupal\Core\Config\ConfigFactoryInterface
@@ -21,10 +27,11 @@ class HelloWorldSalutation {
    * HelloWorldSalutation constructor.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
    */
-  public function __construct(ConfigFactoryInterface $config_factory)
-  {
+  public function __construct(ConfigFactoryInterface $config_factory, EventDispatcherInterface $eventDispatcher) {
     $this->configFactory = $config_factory;
+    $this->eventDispatcher = $eventDispatcher;
   }
 
   /**
@@ -50,7 +57,10 @@ class HelloWorldSalutation {
     }
 
     if($salutation != ""){
-      $salute .= ", " . $salutation;
+      $event = new SalutationEvent();
+      $event->setValue($salutation);
+      $event = $this->eventDispatcher->dispatch(SalutationEvent::EVENT, $event);
+      return $event->getValue();
     }
 
     return $salute;
