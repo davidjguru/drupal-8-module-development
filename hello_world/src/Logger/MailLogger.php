3076506be23da04c451cc6fe6cf4719992215255
davidjguru <davidjguru@gmail.com>
2020-04-23 21:25:54 +0200

    (CFG) / (PHP) - Adds two services and class for Logging and Mailing case use.

--- hello_world/src/Logger/MailLogger.php
+++ hello_world/src/Logger/MailLogger.php
@@ -2,85 +2,109 @@
 
 namespace Drupal\hello_world\Logger;
 
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LogMessageParserInterface;
 use Drupal\Core\Logger\RfcLoggerTrait;
+use Drupal\Core\Logger\RfcLogLevel;
 use Psr\Log\LoggerInterface;
 
 /**
- *  A logger that sends an email when the log type is "error".
+ * A logger that sends an email when the log type is "error".
  */
-class MailLogger implements LoggerInterface{
+class MailLogger implements LoggerInterface {
 
   use RfcLoggerTrait;
 
   /**
+   * @var \Drupal\Core\Logger\LogMessageParserInterface
+   */
+  protected $parser;
+
+  /**
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * MailLogger constructor.
+   *
+   * @param \Drupal\Core\Logger\LogMessageParserInterface $parser
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   */
+  public function __construct(LogMessageParserInterface $parser, ConfigFactoryInterface $config_factory) {
+    $this->parser = $parser;
+    $this->configFactory = $config_factory;
+  }
+
+  /**
    * @{inheritdoc}
    */
-  public function emergency($message, array $context = array())
-  {
+  public function emergency($message, array $context = []) {
     // TODO: Implement emergency() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function alert($message, array $context = array())
-  {
+  public function alert($message, array $context = []) {
     // TODO: Implement alert() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function critical($message, array $context = array())
-  {
+  public function critical($message, array $context = []) {
     // TODO: Implement critical() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function error($message, array $context = array())
-  {
+  public function error($message, array $context = []) {
     // TODO: Implement error() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function warning($message, array $context = array())
-  {
+  public function warning($message, array $context = []) {
     // TODO: Implement warning() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function notice($message, array $context = array())
-  {
+  public function notice($message, array $context = []) {
     // TODO: Implement notice() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function info($message, array $context = array())
-  {
+  public function info($message, array $context = []) {
     // TODO: Implement info() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function debug($message, array $context = array())
-  {
+  public function debug($message, array $context = []) {
     // TODO: Implement debug() method.
   }
 
   /**
    * @{inheritdoc}
    */
-  public function log($level, $message, array $context = array())
-  {
-    // TODO: Implement log() method.
+  public function log($level, $message, array $context = []) {
+    if ($level !== RfcLogLevel::ERROR) {
+      return;
+    }
+    $to = $this->configFactory->get('system.site')->get('mail');
+    $langcode = $this->configFactory->get('system.site')->get('langcode');
+    $variables = $this->parser->parseMessagePlaceholders($message, $context);
+    $markup = new FormattableMarkup($message, $variables);
+    \Drupal::service('plugin.manager.mail')->mail('hello_world', 'hello_world_log', $to, $langcode, ['message' => $markup]);
   }
+
 }
